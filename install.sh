fa8cf664e925d93479c15b1832cc398a4a1b1534
ayatk <git@ayatk.com>
2015-02-13 16:07:04 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/gitattributes ~/.gitattributes
+ln -s ~/dotfiles/git/gitignore_global ~/.gitignore_global
+
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/vim ~/.vim
+
