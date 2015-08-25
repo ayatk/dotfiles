06b48950d9b9202e4627d8c6a2fda558f29a371e
ayatk <ayatk@users.noreply.github.com>
2015-08-25 11:29:41 +0900

    Update 2015/08/25

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 DIR=$(cd $(dirname $0); pwd)
-ln -s DIR/vim/vimrc ~/.vimrc
-ln -s DIR/git/gitconfig ~/.gitconfig
-ln -s DIR/git/gitattributes ~/.gitattributes
-ln -s DIR/git/gitignore_global ~/.gitignore_global
+ln -s $DIR/vim/vimrc ~/.vimrc
+ln -s $DIR/git/gitconfig ~/.gitconfig
+ln -s $DIR/git/gitattributes ~/.gitattributes
+ln -s $DIR/git/gitignore_global ~/.gitignore_global
 
-ln -s DIR/tmux/tmux.conf ~/.tmux.conf
-ln -s DIR/zsh/zshrc ~/.zshrc
-ln -s DIR/vim ~/.vim
+ln -s $DIR/tmux/tmux.conf ~/.tmux.conf
+ln -s $DIR/zsh/zshrc ~/.zshrc
+ln -s $DIR/vim ~/.vim
+ln -s $DIR/bash_profile ~/.bash_profile
 
-sh brewfile
-chsh -s /usr/local/bin/zsh
+# sh brewfile
+## chsh -s /usr/local/bin/zsh
