2b030ac8bdde82ecc9134b42b36d99492ea51b24
ayatk <git@ayatk.com>
2018-12-26 16:03:05 +0900

    インデント修正

--- zsh/functions/history.zsh
+++ zsh/functions/history.zsh
@@ -1,7 +1,7 @@
 if (( $+commands[fzf] )); then
     function select-history() {
         BUFFER=$(history -n -r 1 | uniq | fzf +m --reverse --query "$LBUFFER" --prompt="History > ")
-    zle accept-line
+        zle accept-line
     }
 
     zle -N select-history
