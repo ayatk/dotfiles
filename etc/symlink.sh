999d7dd29deda5001f3eceb931dd69b02679c7b6
ayatk <git@ayatk.com>
2019-08-19 00:03:46 +0900

    fix link script

--- etc/symlink.sh
+++ etc/symlink.sh
@@ -2,14 +2,17 @@
 
 XDG_CONFIG_HOME=$HOME/.config
 
-if [[ ! -f  $HOME/.zshrc ]]; then
-    ln -s $DOTPATH/zsh/.zshrc $HOME/.zshrc
+if [[ -f  $HOME/.zshrc ]]; then
+    mv $HOME/.zshrc $HOME/.zshrc.$(date +'%Y%m%d%H%M%S').backup
 fi 
+
+ln -s $DOTPATH/zsh/.zshrc $HOME/.zshrc
 
 link_dirs=(zsh vim git)
 
-for dir in link_dirs; do
-    if [ ! -d "$DOTPATH" ]; then
-        ln -s $DOTPATH/$dir $XDG_CONFIG_HOME/$dir
+for dir in "${link_dirs[@]}"; do
+    if [ -d "$XDG_CONFIG_HOME/$dir" ]; then
+        mv $XDG_CONFIG_HOME/$dir $XDG_CONFIG_HOME/$dir.$(date +'%Y%m%d%H%M%S').backup
     fi
+    ln -s $DOTPATH/$dir $XDG_CONFIG_HOME/$dir
 done
