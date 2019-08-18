c6c489f267b7af6814c10807eebe20b9bf23414a
ayatk <ayatk@users.noreply.github.com>
2019-08-18 19:23:53 +0900

    update zsh scripts

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,29 +1,13 @@
 #! /bin/env zsh
 
-# Source Prezto.
-if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
-    source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
-fi
+# XDG Base Directory
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
 
-# zoptions
-if [[ -s "${ZDOTDIR:-$HOME}/.zoptions" ]]; then
-    source "${ZDOTDIR:-$HOME}/.zoptions"
-fi
+# zsh home directory
+export ZDOTDIR=$XDG_CONFIG_HOME/zsh
 
-# env settings
-source "${ZDOTDIR:-$HOME}/env/$(uname).zsh"
-
-# functions
-for func (${ZDOTDIR:-$HOME}/functions/*) source $func:a
-
-# zsh completion
-if [ -e /usr/local/share/zsh-completions ]; then
-    fpath=(/usr/local/share/zsh-completions $fpath)
-fi
-
-# zsh site-functions
-fpath=(/usr/local/share/zsh/site-functions $fpath)
-
-if (( $+commands[zprof] )); then
-    zprof | less
+if [[ -s "${ZDOTDIR}/init.zsh" ]]; then
+    source "${ZDOTDIR}/init.zsh"
 fi
---
+++ zsh/env.zsh
@@ -0,0 +1,72 @@
+# history
+export HISTFILE=$XDG_DATA_HOME/zsh/history
+
+# Android
+if (( $+commands[android] )); then
+    export ANDROID_HOME=/usr/local/share/android-sdk
+fi
+
+# atom
+export ATOM_HOME="$XDG_DATA_HOME"/atom
+
+# bundle
+export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME/bundle"
+export BUNDLE_USER_CACHE="$XDG_CACHE_HOME/bundle"
+export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME/bundle"
+
+# gradle 
+if (( $+commands[gradle] )); then
+    export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
+fi
+
+# httpie
+if (( $+commands[http] )); then
+    export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME/httpie"
+fi
+
+# ipython/juniper
+export IPYTHONDIR="$XDG_CONFIG_HOME/jupyter"
+export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
+
+# npm
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+
+# Ruby Gem
+export GEM_HOME="$XDG_DATA_HOME/gem"
+export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem"
+
+# Golang
+if (( $+commands[go] )); then
+    export GOROOT=/usr/local/opt/go/libexec
+    export GOPATH=$HOME
+    export GO111MODULE=on
+    PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+fi
+
+# java
+if (( $+commands[java] )); then
+    export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+    PATH=${JAVA_HOME}/bin:${PATH}
+fi
+
+# openssl
+export PATH=/usr/local/opt/openssl@1.1/bin:$PATH
+
+# pipenv
+if (( $+commands[pipenv] )); then
+    export PIPENV_VENV_IN_PROJECT=1
+fi
+
+# fzf
+if (( $+commands[fzf] )); then
+    export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
+    export FZF_COMPLETION_OPTS='+c -x'
+
+    # Use ag instead of the default find command for listing candidates.
+    # - The first argument to the function is the base path to start traversal
+    # - Note that ag only lists files not directories
+    # - See the source code (completion.{bash,zsh}) for the details.
+    _fzf_compgen_path() {
+        ag -g "" "$1"
+    }
+fi
