f3d6c1372f0e98d6a74ffb72dc6a1f9a808f6ce5
ayatk <git@ayatk.com>
2019-12-31 16:29:06 +0900

    gcloudコマンドとbrewコマンドがない場合は実行しない

--- zsh/functions/gcloud.zsh
+++ zsh/functions/gcloud.zsh
@@ -1,2 +1,4 @@
-source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
-source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+if (( $+commands[gcloud] && $+commands[brew] )); then
+    source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+    source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+fi
