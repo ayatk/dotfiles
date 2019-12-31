b59ce3e0d2ec4a8a622b55b855649e3adf0a0def
ayatk <git@ayatk.com>
2019-12-31 16:44:13 +0900

    feat: gpg-agentは使ってないから消す

--- zsh/functions/gpg2.zsh
+++
@@ -1,6 +0,0 @@
-if (( $+commands[gpg-agent] )); then
-    pgrep -q gpg-agent || eval $(gpg-agent --daemon --write-env-file ${HOME}/.gpg-agent-info)
-    [ -f ${HOME}/.gpg-agent-info ] && source ${HOME}/.gpg-agent-info
-    export GPG_AGENT_INFO
-    export GPG_TTY=$(tty)
-fi
