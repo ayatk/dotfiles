97a8456cf64b663cd5f0f103df6e02e0306abd5b
ayatk <ayatk@users.noreply.github.com>
2020-01-04 19:59:58 +0900

    fix: simlink script

--- etc/symlink.sh
+++ etc/symlink.sh
@@ -2,14 +2,22 @@
 
 XDG_CONFIG_HOME=$HOME/.config
 
-if [[ -f  $HOME/.zshrc ]]; then
-    mv $HOME/.zshrc $HOME/.zshrc.$(date +'%Y%m%d%H%M%S').backup
-fi 
+# files
+dot_files=(
+    ["zshrc"]="zsh/.zshrc" \
+    ["gitconfig"]="git/gitconfig" \
+    ["vimrc"]="vim/vimrc" \
+)
+for file in "${!dot_files[@]}"; do
+    if [[ -f  $HOME/$file ]]; then
+        mv $HOME/$file $HOME/.$file.$(date +'%Y%m%d%H%M%S').backup
+    fi 
 
-ln -s $DOTPATH/zsh/.zshrc $HOME/.zshrc
+    ln -s $DOTPATH/${dot_files[$file]} $HOME/.$file
+done
 
+# directories
 link_dirs=(zsh vim git)
-
 for dir in "${link_dirs[@]}"; do
     if [ -d "$XDG_CONFIG_HOME/$dir" ]; then
         mv $XDG_CONFIG_HOME/$dir $XDG_CONFIG_HOME/$dir.$(date +'%Y%m%d%H%M%S').backup
