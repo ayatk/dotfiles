a0931608e4496b7d30f70b7af49b8ff2fa5f00fc
ayatk <ayatk@users.noreply.github.com>
2020-02-25 10:03:31 +0900

    chore: fix format

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,7 +4,7 @@ PATH="${PATH:+${PATH}:}$DOTPATH/bin"
 
 # Android
 if (( $+commands[android] )); then
-    export ANDROID_HOME=/usr/local/share/android-sdk
+  export ANDROID_HOME=/usr/local/share/android-sdk
 fi
 
 # docker
@@ -15,14 +15,14 @@ export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME/bundle"
 export BUNDLE_USER_CACHE="$XDG_CACHE_HOME/bundle"
 export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME/bundle"
 
-# gradle 
+# gradle
 if (( $+commands[gradle] )); then
-    export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
+  export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
 fi
 
 # httpie
 if (( $+commands[http] )); then
-    export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME/httpie"
+  export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME/httpie"
 fi
 
 # ipython/juniper
@@ -38,10 +38,10 @@ export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem"
 
 # Golang
 if (( $+commands[go] )); then
-    export GOROOT=/usr/local/opt/go/libexec
-    export GOPATH=$HOME
-    export GO111MODULE=on
-    PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+  export GOROOT=/usr/local/opt/go/libexec
+  export GOPATH=$HOME
+  export GO111MODULE=on
+  PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 fi
 
 # openssl
@@ -55,19 +55,19 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 # pipenv
 if (( $+commands[pipenv] )); then
-    export PIPENV_VENV_IN_PROJECT=1
+  export PIPENV_VENV_IN_PROJECT=1
 fi
 
 # fzf
 if (( $+commands[fzf] )); then
-    export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
-    export FZF_COMPLETION_OPTS='+c -x'
+  export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
+  export FZF_COMPLETION_OPTS='+c -x'
 
-    # Use ag instead of the default find command for listing candidates.
-    # - The first argument to the function is the base path to start traversal
-    # - Note that ag only lists files not directories
-    # - See the source code (completion.{bash,zsh}) for the details.
-    _fzf_compgen_path() {
-        ag -g "" "$1"
-    }
+  # Use ag instead of the default find command for listing candidates.
+  # - The first argument to the function is the base path to start traversal
+  # - Note that ag only lists files not directories
+  # - See the source code (completion.{bash,zsh}) for the details.
+  _fzf_compgen_path() {
+    ag -g "" "$1"
+  }
 fi
--- zsh/functions/completion.zsh
+++ zsh/functions/completion.zsh
@@ -1,13 +1,13 @@
 # hub alias
 if (( $+commands[hub] )); then
-    eval "$(hub alias -s)"
+  eval "$(hub alias -s)"
 fi
 
 # direnv
 if (( $+commands[direnv] )); then
-    eval "$(direnv hook zsh)"
+  eval "$(direnv hook zsh)"
 fi
 
 if (( $+commands[kubectl] )); then
-    source <(kubectl completion zsh)
+  source <(kubectl completion zsh)
 fi
