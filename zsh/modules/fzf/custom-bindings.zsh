66a329b5f0a76007719f36ae8e001818e9fa90bf
ayatk <ayatk@users.noreply.github.com>
2020-03-10 00:27:35 +0900

    cdした後にgit syncをするようにした

--- zsh/modules/fzf/custom-bindings.zsh
+++ zsh/modules/fzf/custom-bindings.zsh
@@ -1,7 +1,9 @@
 if (( $+commands[ghq] )); then
     function _fast_move_git_repo() {
         local dir
-        dir=$(ghq list >/dev/null | fzf --reverse +m) && cd $(ghq root)/$dir
+        dir=$(ghq list >/dev/null | fzf --reverse +m) && \
+            cd $(ghq root)/$dir && \
+            git sync
         zle accept-line
     }
 
