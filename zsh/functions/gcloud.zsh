f78fcbd282d37a13d6951a887115d0d4140e1cd4
ayatk <git@ayatk.com>
2020-12-13 22:45:53 +0900

    fix

--- zsh/functions/gcloud.zsh
+++ zsh/functions/gcloud.zsh
@@ -1,4 +1,5 @@
 if (( $+commands[gcloud] && $+commands[brew] )); then
+  export CLOUDSDK_PYTHON="/usr/local/opt/python@3.8/libexec/bin/python"
   source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
   source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
 fi
