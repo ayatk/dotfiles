89ab694a20efc0008a5ad82b703888024bd0cc1f
ayatk <ayatk@users.noreply.github.com>
2021-12-10 11:23:49 +0900

    pyenvとjupyterがある場合にのみ環境変数を初期化する

--- zsh/envs/python.zsh
+++ zsh/envs/python.zsh
@@ -4,10 +4,14 @@ if (( $+commands[pipenv] )); then
 fi
 
 # pyenv
-export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
+if (( $+commands[pyenv] )); then
+  export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
+fi
 
 # jupyter
-export IPYTHONDIR="$XDG_CONFIG_HOME/jupyter"
-export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
+if (( $+commands[jupyter] )); then
+  export IPYTHONDIR="$XDG_CONFIG_HOME/jupyter"
+  export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
+fi
