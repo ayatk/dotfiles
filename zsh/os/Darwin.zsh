868c03de1aa2bc9fc338a98b07e0a978c42b9fb7
ayatk <ayatk@users.noreply.github.com>
2022-07-26 14:33:39 +0900

    M1macでもbrewを使えるようにする

--- zsh/os/Darwin.zsh
+++ zsh/os/Darwin.zsh
@@ -1,4 +1,8 @@
 if (( $+commands[brew] )); then
+  if [[ $(uname -m) == "arm64" ]]; then
+     eval $(/opt/homebrew/bin/brew shellenv)
+  fi
+
   function update() {
     brew update
     brew upgrade
