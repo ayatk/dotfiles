0556360519e28019935fbf78d1ce4ff87ee1a036
ayatk <git@ayatk.com>
2022-07-30 14:58:00 +0900

    m1 mac対応

--- zsh/envs/golang.zsh
+++ zsh/envs/golang.zsh
@@ -1,7 +1,5 @@
 # Golang
 if (( $+commands[go] )); then
-  export GOROOT=/usr/local/opt/go/libexec
-  export GO111MODULE=on
-  PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+  PATH=$PATH:$GOPATH/bin
 fi
 
--- zsh/os/Darwin.zsh
+++ zsh/os/Darwin.zsh
@@ -1,8 +1,4 @@
 if (( $+commands[brew] )); then
-  if [[ $(uname -m) == "arm64" ]]; then
-     eval $(/opt/homebrew/bin/brew shellenv)
-  fi
-
   function update() {
     brew update
     brew upgrade
@@ -10,20 +6,3 @@ if (( $+commands[brew] )); then
     brew cleanup
   }
 fi
-
-# java
-export PATH=/usr/local/opt/openjdk/bin:$PATH
-
-# Android
-if (( $+commands[android] )); then
-  export ANDROID_HOME=/usr/local/share/android-sdk
-fi
-
-# openssl
-export PATH=/usr/local/opt/openssl@1.1/bin:$PATH
-
-if (( $+commands[go] )); then
-  export GOROOT=/usr/local/opt/go/libexec
-fi
-
-export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
