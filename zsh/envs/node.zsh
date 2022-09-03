aba2714817e0a1653aae9ca5d4deab89859407a8
ayatk <ayatk@users.noreply.github.com>
2022-09-04 00:50:52 +0900

    コマンドがないと設定されない環境変数をデフォルトで設定するようにした

--- zsh/envs/node.zsh
+++ zsh/envs/node.zsh
@@ -2,7 +2,5 @@
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
 
 # n
-if (( $+commands[n] )); then
-  export N_PREFIX=$XDG_DATA_HOME/n
-  export PATH="$N_PREFIX/bin:$PATH"
-fi
+export N_PREFIX=$XDG_DATA_HOME/n
+export PATH="$N_PREFIX/bin:$PATH"
