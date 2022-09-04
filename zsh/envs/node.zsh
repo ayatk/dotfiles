6cb5899cb2f2c9ed2cac8b3afc7a264715dbc0fe
ayatk <git@ayatk.com>
2022-09-04 13:31:11 +0900

    add npm dir

--- zsh/envs/node.zsh
+++ zsh/envs/node.zsh
@@ -1,5 +1,9 @@
 # npm
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+export PATH="${XDG_DATA_HOME}/npm/bin:$PATH"
+
+# node
+export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
 
 # n
 export N_PREFIX=$XDG_DATA_HOME/n
