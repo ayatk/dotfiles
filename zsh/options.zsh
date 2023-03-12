149fd413bd24cfdead970a32ad07a87aaeb938be
ayatk <ayatk@users.noreply.github.com>
2023-03-12 19:24:21 +0900

    zpreztoで定義されている不要なoptionを削除

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,20 +1,11 @@
 # 日本語ファイル名を表示可能にする
 setopt print_eight_bit
 
-# 履歴を共有する
-setopt share_history
-
 # beep を無効にする
 setopt no_beep
 
 # フローコントロールを無効にする
 setopt no_flow_control
-
-# '#' 以降をコメントとして扱う
-setopt interactive_comments
-
-# 補完で小文字でも大文字にマッチさせる
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # ../ の後は今いるディレクトリを補完しない
 zstyle ':completion:*' ignore-parents parent pwd ..
@@ -28,14 +19,3 @@ setopt auto_param_keys
 # 範囲指定できるようにする
 # 例 : mkdir {1-3} で フォルダ1, 2, 3を作れる
 setopt brace_ccl
-
-# 語の途中でもカーソル位置で補完
-setopt complete_in_word
-
-# 拡張グロブで補完(~とか^とか。例えばless *.txt~memo.txt ならmemo.txt 以外の *.txt にマッチ)
-setopt extended_glob
-
-# 補完
-zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "${XDG_CACHE_HOME}/zsh/compcache"
-compinit -d $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION
