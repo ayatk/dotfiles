6120ca04bd36cf97cbb2c74411c12f6d21bac5cd
ayatk <ayatk@users.noreply.github.com>
2023-03-12 16:03:51 +0900

    fix asdf environment

--- zsh/envs/asdf.zsh
+++ zsh/envs/asdf.zsh
@@ -1,4 +1,10 @@
 export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdf/config"
 export ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
 
-source /opt/homebrew/opt/asdf/libexec/asdf.sh
+if [[ -s "/opt/homebrew/opt/asdf/libexec/asdf.sh" ]]; then
+  source /opt/homebrew/opt/asdf/libexec/asdf.sh
+else
+  export ASDF_DIR="$XDG_DATA_HOME/asdf"
+  source $ASDF_DIR/asdf.sh
+fi
+
